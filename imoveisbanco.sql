d96d10e5a54ed2d31faf5df0f8faf46d8b713779
HELOUISE DAYANE DOS SANTOS SILVA <109963008+HelouiseDayane@users.noreply.github.com>
2023-02-04 10:16:55 -0300

    atualização do banco
    banco completo

--- imoveisbanco.sql
+++ imoveisbanco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Fev-2023 às 13:40
+-- Tempo de geração: 04-Fev-2023 às 14:15
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.15
 
@@ -20,6 +20,82 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `siteimobiliario`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bairros`
+--
+
+CREATE TABLE `bairros` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `bairros`
+--
+
+INSERT INTO `bairros` (`id`, `nome`) VALUES
+(2, 'Lagoa Nova'),
+(3, 'Capim Macio'),
+(5, 'Ponta Negra');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidades`
+--
+
+CREATE TABLE `cidades` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cidades`
+--
+
+INSERT INTO `cidades` (`id`, `nome`) VALUES
+(1, 'Natal'),
+(2, 'Parnamirim'),
+(3, 'Extremoz');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `config`
+--
+
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL,
+  `nome_sistema` varchar(50) NOT NULL,
+  `telefone_sistema` varchar(22) NOT NULL,
+  `email_sistema` varchar(50) NOT NULL,
+  `cnpj_sistema` varchar(60) DEFAULT NULL,
+  `logo` varchar(30) DEFAULT NULL,
+  `icone` varchar(30) DEFAULT NULL,
+  `logo_rel` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `config`
+--
+
+INSERT INTO `config` (`id`, `nome_sistema`, `telefone_sistema`, `email_sistema`, `cnpj_sistema`, `logo`, `icone`, `logo_rel`) VALUES
+(1, 'Site Imobiliario', '(84) 99953-3663', 'helouisedayane@gmail.com', NULL, 'logo.png', 'favicon.ico', 'logo.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `id` int(11) NOT NULL,
+  `imoveis_id` int(11) NOT NULL,
+  `foto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,9 +129,77 @@ INSERT INTO `imoveis` (`id`, `titulo`, `imoveis_tipo`, `padrao`, `imoveis_bairro
 (11, 'Quartos de aluguel', 0, 'Alto', 2, '800,00', 2, 'erfreferfer', 1, 'Aluguel'),
 (12, 'Puxadinho 1', 8, 'Básico', 2, '700.00', 2, 'sacascascasczxz', 1, 'Aluguel');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipos`
+--
+
+CREATE TABLE `tipos` (
+  `id` int(11) NOT NULL,
+  `tipo_imoveis` text NOT NULL,
+  `padrao_imoveis` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tipos`
+--
+
+INSERT INTO `tipos` (`id`, `tipo_imoveis`, `padrao_imoveis`) VALUES
+(5, 'Apartamento', ''),
+(8, 'Kitnet', ''),
+(9, 'Triplex', ''),
+(10, 'Duplex', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `nivel` varchar(100) NOT NULL,
+  `foto` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `nivel`, `foto`) VALUES
+(1, 'Helouise', 'helouisedayane@gmail.com', '202cb962ac59075b964b07152d234b70', 'Administrador', '');
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `bairros`
+--
+ALTER TABLE `bairros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `cidades`
+--
+ALTER TABLE `cidades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `imoveis`
@@ -64,14 +208,62 @@ ALTER TABLE `imoveis`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `tipos`
+--
+ALTER TABLE `tipos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `bairros`
+--
+ALTER TABLE `bairros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `cidades`
+--
+ALTER TABLE `cidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `config`
+--
+ALTER TABLE `config`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `imoveis`
 --
 ALTER TABLE `imoveis`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `tipos`
+--
+ALTER TABLE `tipos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
